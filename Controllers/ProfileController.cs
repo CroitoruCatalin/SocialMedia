53ee934609110d049dfff5e2cdb76c93f905cfdc
CroitoruCatalin <160837031+CroitoruCatalin@users.noreply.github.com>
2024-06-27 21:20:55 +0300

    Added custom user-uploaded profile pictures

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Models;
+using SocialMedia.Services;
+using System.Threading.Tasks;
+
+[Authorize]
+public class ProfileController : Controller
+{
+    private readonly UserManager<User> _userManager;
+    private readonly ImageService _imageService;
+
+    public ProfileController(UserManager<User> userManager, ImageService imageService)
+    {
+        _userManager = userManager;
+        _imageService = imageService;
+    }
+
+    [HttpGet]
+    public IActionResult EditProfile()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> EditProfile(IFormFile profilePicture)
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return NotFound("User not found.");
+        }
+
+        if (profilePicture != null)
+        {
+            var image = await _imageService.UploadImageAsync(profilePicture, user.Id);
+            user.ProfilePictureId = image.ImageId;
+            await _userManager.UpdateAsync(user);
+        }
+
+        return RedirectToAction("EditProfile");
+    }
+}
---
+++ Services/ImageService.cs
@@ -0,0 +1,50 @@
+﻿using SocialMedia.Models;
+
+namespace SocialMedia.Services
+{
+    public class ImageService
+    {
+        private readonly SocialContext _context;
+
+        public ImageService(SocialContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Image> UploadImageAsync(IFormFile file, string userId)
+        {
+            if (file == null || file.Length == 0)
+                throw new ArgumentException("File is empty", nameof(file));
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await file.CopyToAsync(memoryStream);
+                var image = new Image
+                {
+                    FileName = file.FileName,
+                    ContentType = file.ContentType,
+                    Data = memoryStream.ToArray(),
+                    UserId = userId,
+                    UploadDate = DateTime.UtcNow
+                };
+                _context.Images.Add(image);
+                await _context.SaveChangesAsync();
+                return image;
+            }
+        }
+        public async Task<Image> AssignDefaultProfilePicture(string userId)
+        {
+            var defaultImage = new Image
+            {
+                FileName = "default.png",
+                ContentType = "image/png",
+                Data = await File.ReadAllBytesAsync("wwwroot/assets/images/default.png"), // Assume you have a default image in this path
+                UserId = userId,
+                UploadDate = DateTime.UtcNow
+            };
+            _context.Images.Add(defaultImage);
+            await _context.SaveChangesAsync();
+            return defaultImage;
+        }
+    }
+}
