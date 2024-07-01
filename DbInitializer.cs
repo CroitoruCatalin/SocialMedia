3485388f19471085918c0ba9d0dda0169b396733
CroitoruCatalin <160837031+CroitoruCatalin@users.noreply.github.com>
2024-07-01 09:21:43 +0300

    adding dbinitializer

---
+++ DbInitializer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using SocialMedia.Models;
+
+public static class DbInitializer
+{
+    public static async Task Initialize(IServiceProvider serviceProvider)
+    {
+        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+
+        string[] roleNames = { "Admin", "User" };
+        IdentityResult roleResult;
+
+        foreach (var roleName in roleNames)
+        {
+            var roleExist = await roleManager.RoleExistsAsync(roleName);
+            if (!roleExist)
+            {
+                roleResult = await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+        }
+        var adminUser = await userManager.FindByEmailAsync("admin@mail.com");
+        if (adminUser == null)
+        {
+            adminUser = new User()
+            {
+                FullName = "AdminUser",
+                Email = "admin@mail.com",
+            };
+            await userManager.CreateAsync(adminUser, "admin@mail.com");
+        }
+
+        if (!await userManager.IsInRoleAsync(adminUser, "Admin"))
+        {
+            await userManager.AddToRoleAsync(adminUser, "Admin");
+        }
+    }
+}
